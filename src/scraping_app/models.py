2258ae93cb2d14868f4ac3f60cb0cc5af1c73e75
riasnoi <vladimir.riasnoi@gmail.com>
2022-02-17 16:59:00 +0300

    A language-model has been added to project

--- src/scraping_app/models.py
+++ src/scraping_app/models.py
@@ -1,12 +1,23 @@
 from django.db import models
 
 class City(models.Model):
-    name = models.CharField(max_length=50, verbose_name='Название города')
-    slug = models.CharField(max_length=50, blank=True)
+    name = models.CharField(max_length=50, verbose_name='Название города', unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
 
     class Meta:
         verbose_name = 'Город'
         verbose_name_plural = 'Города'
+
+    def __str__(self):
+        return self.name
+
+class Language(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Язык программирования', unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
+
+    class Meta:
+        verbose_name = 'Язык программирования'
+        verbose_name_plural = 'Языки программирования'
 
     def __str__(self):
         return self.name
