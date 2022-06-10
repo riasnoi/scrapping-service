e4214d73cd2d3ef413b2a326563651eb52286790
riasnoi <vladimir.riasnoi@gmail.com>
2022-06-10 21:47:05 +0300

    have edited a design, added a paganation on page with vacancies

--- src/scraping_app/views.py
+++ src/scraping_app/views.py
@@ -1,21 +1,35 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 from .forms import FindForm
 from .models import Vacancy
 
 
-def vacancy_view(request):
+def main_view(request):
+    form = FindForm()
+    return render(request, 'scraping_app/home.html', {'form': form})
+
+
+def list_view(request):
+    form = FindForm()
     language = request.GET.get('language')
     city = request.GET.get('city')
-
-    form = FindForm()
     qs = []
-    _filter = {}
+
+    context = {'city': city, 'language': language, 'form': form}
+
     if city or language:
+        _filter = {}
         if city:
             _filter['city__slug'] = city
         if language:
             _filter['language__slug'] = language
 
-    qs = Vacancy.objects.filter(**_filter)
-    return render(request, 'scraping_app/home.html', {'object_list': qs, 'form': form})
+        qs = Vacancy.objects.filter(**_filter)
+        paginator = Paginator(qs, 10)
+
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        context['object_list'] = page_obj
+
+    return render(request, 'scraping_app/list.html', context)
--- src/scrapping_service/urls.py
+++ src/scrapping_service/urls.py
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from scraping_app.views import vacancy_view
+from scraping_app.views import main_view, list_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', vacancy_view),
-]
+    path('vacancy/', list_view, name='list'),
+    path('', main_view, name='main')
+] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
